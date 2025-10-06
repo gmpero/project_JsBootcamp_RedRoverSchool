851d58b6f070e5a4bded1818fb4302b230c3c127
Anastasiya <anastasiyazenevich@gmail.com>
2025-10-06 15:29:15 +0300

    fix: rename the first parameter in function and add a default message if a second parameter is missing (solution to the first task)

--- meetings/meeting.10.05.js
+++ meetings/meeting.10.05.js
@@ -3,14 +3,14 @@
 // 1. Поиск ключа по значению
 // Найди ключ по значению (первое вхождение)
 // findKey({name: "John", age: 30}, "John") // → "name"
-function findKey(arr, string) {
-    for(let [key, value] of Object.entries(arr)) {
+function findKey(obj, string) {
+    for(let [key, value] of Object.entries(obj)) {
         if (value === string) {
             return key;
         }
-    }
-}
-console.log(findKey({name: "John", age: 30}, "John"));
+    } return 'The value is not found'
+};
+console.log(findKey({name: "John", age: 30}, "Anna"));
 
 // 2. Поменяй ключи и значения местами
 // invertObject({a: "x", b: "y"}) // → {x: "a", y: "b"}
